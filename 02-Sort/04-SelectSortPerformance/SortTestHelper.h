c847f75a8c267374e0042224bd36ec71efcfb22a
周超 <netstan@163.com>
2024-05-18 22:47:36 +0800

    sort test time

---
+++ 02-Sort/04-SelectSortPerformance/SortTestHelper.h
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+namespace SortTestHelper
+{
+    // 生成有n个元素的随机数组,每个元素的随机范围为[rangeL, rangeR]
+    /**
+     * @brief 生成随机数组
+     *
+     * 在指定范围内生成一个长度为n的随机整数数组，并返回该数组的指针。
+     *
+     * @param n 数组长度
+     * @param rangeL 范围下限（包含）
+     * @param rangeR 范围上限（包含）
+     *
+     * @return 随机数组指针
+     */
+    int *generateRandomArray(int n, int rangeL, int rangeR) {
+        // 断言范围左边界小于等于右边界
+        assert(rangeL <= rangeR);
+
+        // 动态分配整型数组
+        int *arr = new int[n];
+
+        // 初始化随机数种子
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+            // 生成范围在[rangeL, rangeR]内的随机整数
+            arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+        return arr;
+    }
+
+    // 打印arr数组的所有内容
+    template<typename T>
+    void printArray(T arr[], int n) {
+
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+
+        return;
+    }
+
+    // 测试sort排序算法排序arr数组所得到结果的正确性和算法运行时间
+    // * 使用VS编码的同学, 对于函数指针的写法和调用方法可能和课程中介绍的有所不同;
+    // * 并且不同版本的VS, 其具体语法可能也有差异, 这是因为VS的编译器不完全是按照C++的标准实现的;
+    template<typename T>
+    void testSort(const string &sortName, void (*sort)(T[], int), T arr[], int n) {
+        // 记录排序开始时间
+        clock_t startTime = clock();
+
+        // 调用传入的排序函数进行排序
+        sort(arr, n);
+
+        // 记录排序结束时间
+        clock_t endTime = clock();
+
+        // 断言排序结果是否正确
+        assert(isSorted(arr, n));
+
+        // 输出排序函数名称和排序所花费的时间
+        cout << sortName << " : " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+
+        return;
+    }
+
+    
+} // namespace SortTestHelper
