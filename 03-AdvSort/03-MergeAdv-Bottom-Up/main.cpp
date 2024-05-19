6b9d06d7417e836d050e9039da489c37fc39109e
周超 <netstan@163.com>
2024-05-19 15:02:28 +0800

    自底向上排序

---
+++ 03-AdvSort/03-MergeAdv-Bottom-Up/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include "SortTestHelper.h"
+
+using namespace std;
+
+
+template<typename T>
+// 对arr[l...r]范围的数组进行插入排序
+void insertSort(T arr[], int l, int r){
+
+    for( int i = l+1 ; i <= r ; i ++ ) {
+
+        T e = arr[i];
+        int j;
+        for (j = i; j > l && arr[j-1] > e; j--)
+            arr[j] = arr[j-1];
+        arr[j] = e;
+    }
+
+    return;
+}
+
+template<typename  T>
+void __merge(T arr[], int l, int mid, int r){
+
+    //* VS不支持动态长度数组, 即不能使用 T aux[r-l+1]的方式申请aux的空间
+    //* 使用VS的同学, 请使用new的方式申请aux空间
+    //* 使用new申请空间, 不要忘了在__merge函数的最后, delete掉申请的空间:)
+    // T aux[r-l+1];
+    T *aux = new T[r-l+1];
+
+    for( int i = l ; i <= r; i ++ )
+        aux[i-l] = arr[i];
+
+    // 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
+    int i = l, j = mid+1;
+    for( int k = l ; k <= r; k ++ ){
+
+        if( i > mid ){  // 如果左半部分元素已经全部处理完毕
+            arr[k] = aux[j-l]; j ++;
+        }
+        else if( j > r ){  // 如果右半部分元素已经全部处理完毕
+            arr[k] = aux[i-l]; i ++;
+        }
+        else if( aux[i-l] < aux[j-l] ) {  // 左半部分所指元素 < 右半部分所指元素
+            arr[k] = aux[i-l]; i ++;
+        }
+        else{  // 左半部分所指元素 >= 右半部分所指元素
+            arr[k] = aux[j-l]; j ++;
+        }
+    }
+
+    //delete[] aux;
+}
+
+template<typename T>
+void mergeSortBU(T arr[], int n) {
+
+    // 初始化子数组的大小为1
+    for(int sz=1; sz<n; sz+=sz) {
+        // 遍历数组，每次合并两个相邻的子数组
+        for(int i=0; i+sz<n; i+=sz+sz) {
+
+            // 合并两个有序数组
+            // arr[i]到arr[i+sz-1]为一个有序数组
+            // arr[i+sz]到arr[min(i+sz+sz-1,n-1)]为另一个有序数组
+            // 将这两个有序数组合并为一个有序数组
+            // 合并两个有序数组
+            __merge(arr,i,i+sz-1,min(i+sz+sz-1,n-1));
+        }
+    }
+}
+
+// 使用优化的归并排序算法, 对arr[l...r]的范围进行排序
+template<typename T>
+void __mergeSort(T arr[], int l, int r){
+
+    // 对于小规模数组, 使用插入排序
+    if( r - l <= 15 ){
+        insertSort(arr, l, r);
+        return;
+    }
+
+    int mid = (l+r)/2;
+    __mergeSort(arr, l, mid);
+    __mergeSort(arr, mid+1, r);
+
+    // 对于arr[mid] <= arr[mid+1]的情况,不进行merge
+    // 对于近乎有序的数组非常有效,但是对于一般情况,有一定的性能损失
+    if( arr[mid] > arr[mid+1] )
+        __merge(arr, l, mid, r);
+}
+
+template<typename T>
+void mergeSort(T arr[], int n){
+
+    __mergeSort( arr , 0 , n-1 );
+}
+
+int main() {
+
+    // Merge Sort是我们学习的第一个O(nlogn)复杂度的算法
+    // 可以在1秒之内轻松处理100万数量级的数据
+    // 注意：不要轻易尝试使用SelectionSort, InsertionSort或者BubbleSort处理100万级的数据
+    // 否则，你就见识了O(n^2)的算法和O(nlogn)算法的本质差异：）
+    int n = 10000000;
+
+    // 测试1 一般性测试
+    cout<<"Test for random array, size = "<<n<<", random range [0, "<<n<<"]"<<endl;
+    int* arr1 = SortTestHelper::generateRandomArray(n,0,n);
+    int* arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+    SortTestHelper::testSort("Merge Sort Bottom Up", mergeSortBU, arr2, n);
+
+    delete[] arr1;
+    delete[] arr2;
+
+    cout<<endl;
+    return 0;
+}
+
+
